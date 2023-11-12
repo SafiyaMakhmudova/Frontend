f37d0132c45410c83618f5815e5b4eeb90313de7
miss Safiya <maxmudovarruxsora@gmail.com>
2023-11-12 10:20:06 +0500

    vuejs

---
+++ vite-project/src/App.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <!-- body -->
+<div class="container">
+  <!--navbar  -->
+<nav class="header">
+  <div >
+            <a class="" href="#">Nexcent</a>
+    
+  </div>
+        <div class="right">
+            <ul class="links">
+                <li><a class="">Home</a></li>
+                <li><a class="" href="#">Features</a></li>
+                <li><a class="" href="#">Community</a></li>
+                <li><a class="" href="#">Blog</a></li>
+                <li><a class="" href="#">Pricing</a></li>
+            </ul>
+            <button class="btn">Register Now</button>
+        </div>
+</nav>
+  <!-- hero -->
+  <div>
+    hero
+  </div>
+  
+</div>
+</template>
+
+<style scoped></style>

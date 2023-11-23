8d8984d7fb05e8eb35ccbef392c756d944acef3b
miss Safiya <maxmudovarruxsora@gmail.com>
2023-11-23 22:59:56 +0500

    vuejs/taillwind

--- todos/src/components/index.js
+++ todos/src/components/index.js
@@ -1,14 +1,134 @@
-import {ref, onMounted} from 'vue'
+import {ref, onMounted, reactive} from 'vue'
 
 export function useTodo() {
     const todos = ref([])
+    const editingId = ref(null);
+    const showUpdate = ref('false')
+    
+    const todoo = reactive({
+        title: ' ',
+        completed:false,
+    })
+
+    const updateTodo = reactive({
+        title: ''
+    })
+
+    function resetTodo () {
+        todoo.title = ''
+    }
 
     async function fetchUrl () {
-       const res = await fetch( `http://localhost:3000/todos`)
-        const data =  await res.json() 
-        todos.value = data
-        console.log(todos.value);
+        try {
+            const res = await fetch( `http://localhost:3000/todos?_sort=id&_order=desc`)
+            const data =  await res.json() 
+            todos.value = data
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    async function addItem () {
+        try{
+            if(todoo.title.length !=1 ) {
+                
+                fetch(`http://localhost:3000/todos`, {
+                method: 'POST',
+                headers : {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                        
+                    body: JSON.stringify(todoo),
+                })
+                .then(res => res.json())
+                .then((item) => {
+                if (!item.id) {
+                    return;
+                }
+                
+                fetchUrl();
+                resetTodo()
+                })
+            }
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    async function deleteItem (id) {
+        try {
+            fetch( `http://localhost:3000/todos/${id}`, {
+                method:'DELETE'
+            })
+                
+            .then(res => res.json())
+            .then(() => {
+            fetchUrl();
+            })
+
+        } catch(error) {
+            console.log(error);
+        }
+    }
+    async function checkupdated (id, completed, title) {
+        try {
+
+            const check = reactive({
+                title:title,
+                completed:!completed,
+            })
+            console.log(check.completed, check.title);
+
+            fetch(`http://localhost:3000/todos/${id}`, {
+                method: 'PUT',
+                headers:  { 
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(check),
+            })
+            .then(res => res.json())
+            .then((item) => {
+              if (!item.id) {
+                return;
+              }
+              fetchUrl();
+            })
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    function Update (item) {
+        console.log(showUpdate.value);
+        showUpdate.value = true,
+        editingId.value = item.id,
+        updateTodo.title = item.title
+    }
+
         
+    function onSubmitUpdateModal() {
+       try {
+        fetch(`http://localhost:3000/todos/${editingId.value}`, {
+            method: 'PUT',
+            headers:  { 
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updateTodo),
+        })
+            .then(res => res.json())
+            .then((item) => {
+            if (!item.id) {
+                return;
+            }
+            showUpdate.value = false;
+            fetchUrl();
+            })
+       } catch (error) {
+        console.log(error);
+       }
     }
 
     onMounted (()=> {
@@ -19,5 +139,12 @@ export function useTodo() {
     return {
         fetchUrl,
         todos,
+        todoo,
+        addItem,
+        showUpdate,
+        deleteItem,
+        checkupdated,
+        Update,
+        onSubmitUpdateModal
     }
 }
